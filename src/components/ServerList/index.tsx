d64c2f1d8ecc12a08774eed826cc997238c0f97c
Henrique Pereira <henrique.pereira@sapium.com.br>
2020-06-22 22:23:09 -0300

    Finish

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import ServerButton from '../ServerButton';
+
+import { Container, Separator } from "./styles";
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+      <ServerButton isHome />
+
+      <Separator />
+
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton />
+      <ServerButton mentions={300}/>
+      <ServerButton />
+    </Container>
+  );
+};
+
+export default ServerList;
